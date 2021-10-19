2f3289618c2c03553c1309792b393ff859af7794
Luis Flores <lf016158@gmail.com>
2021-10-19 12:38:52 -0600

    Se agrego la plantilla para el modo administrador

---
+++ Administrador/template/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer full-reset">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <h4 class="all-tittles">Acerca de</h4>
+                        <p>
+                            Esta diseñado para manejo de clientes y prestamos a largo y corto plazo.
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <h4 class="all-tittles">Desarrollador</h4>
+                        <ul class="list-unstyled">
+                            <li><i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; Luis Flores <a href="https://www.facebook.com/"><i  class="zmdi zmdi-facebook zmdi-hc-fw footer-social"> </i></a><a href="https://twitter.com/home"><i class="zmdi zmdi-twitter zmdi-hc-fw footer-social"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright full-reset all-tittles">© <?php $Year = date("Y"); echo $Year; ?> Luis Flores</div>
+        </footer>
+    </div>
+</body>
+</html>
